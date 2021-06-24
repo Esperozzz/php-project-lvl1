b01de61b337a2967575b6172d34f0c43ffe03211
esperozzz <esperoz9@gmail.com>
2021-06-24 19:52:21 +0300

    add math functions

---
+++ src/Brain/Math.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Brain\Math;
+
+/**
+ * Сложение двух чисел
+ */
+function addition(int $num1, int $num2): int
+{
+    return $num1 + $num2;
+}
+
+/**
+ * Вычитание одного числа из другого
+ */
+function substraction(int $num1, int $num2): int
+{
+    return $num1 - $num2;
+}
+
+/**
+ * Умножение одного числа на другое
+ */
+function multiplied(int $num1, int $num2): int
+{
+    return $num1 * $num2;
+}
